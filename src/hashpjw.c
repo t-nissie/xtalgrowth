423dbaea27b50ef01ad6e24fcab3d1c69cf37141
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2010-11-16 04:08:50 +0000

    hashpjw.c: not h%PRIME but h
    git-svn-id: https://svn.code.sf.net/p/loto/code/xtalgrowth/trunk@632 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/hashpjw.c
+++ src/hashpjw.c
@@ -1,9 +1,12 @@
 /* hashpjw.c
-   Time-stamp: <2010-11-13 21:15:18 takeshi>
-   Author: Takeshi NISHIMATSU
-   Reference:
+
+   P. J. Weinberger's hash function from the Dragon Book
+   (Alfred V. Aho, Ravi Sethi, and Jeffrey D. Ullman:
+   "Compilers: Principles, Techniques, and Tools" (1986),
+   ISBN:0-201-10088-6, Addison Wesley).
+
+   Time-stamp: <2010-11-16 13:07:18 takeshi>
 */
-#define PRIME 211
 int hashpjw(char *s)
 {
   char *p;
@@ -11,10 +14,10 @@ int hashpjw(char *s)
   unsigned g;
   for (p=s; *p; p++) {
     h = (h<<4) + (*p);
-    if (g=h & 0xf0000000) {
+    if (g = h&0xf0000000) {
       h = h^(g>>24);
       h = h^g;
     }
   }
-  return h % PRIME;
+  return h;
 }
