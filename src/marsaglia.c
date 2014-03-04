acb0dd0644d6f41b0311d3db52dbf6e50ab93430
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-03-04 03:26:50 +0000

    add TESTS
    git-svn-id: https://svn.code.sf.net/p/loto/code/xtalgrowth/trunk@2078 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/marsaglia.c
+++ src/marsaglia.c
@@ -1,24 +1,28 @@
 /* marsaglia.c tests uni64.c
-   Time-stamp: <2010-11-16 19:44:58 takeshi>
+   Time-stamp: <2014-03-04 12:22:45 takeshi>
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include "uni64.h"
 int main()
 {
  double x;
  int i;
+ static double a[5] = {
+   5534819329886631.0,
+   7222984478804879.0,
+   3591084909746267.0,
+   6938004922860200.0,
+   3518398318353033.0 };
  fillU(123456789,987654321);
- for (i=1;i<=10000000;i++) x=uni64();
- for(i=1;i<=5;i++) printf("%18.0f \n",uni64()*9007199254740992.0);
+ for (i=0;i<10000000;i++) x=uni64();
+ for (i=0;i<5;i++) {
+   x = uni64();
+   if (x*9007199254740992.0 != a[i]) exit(i+1);
+ }
  return 0;
 }
-/* The correct output:
-   5534819329886631
-   7222984478804879
-   3591084909746267
-   6938004922860200
-   3518398318353033
-
+/*
 Local variables:
   compile-command: "gcc -O0 -Wall -g -o marsaglia marsaglia.c uni64.c && ./marsaglia"
 End:
--- src/marsaglia2d.c
+++ src/marsaglia2d.c
@@ -1,17 +1,20 @@
 /* marsaglia2d.c tests uni64.c
-   Time-stamp: <2010-11-21 13:14:36 takeshi>
+   Time-stamp: <2014-03-04 12:10:50 takeshi>
 */
 #include <stdio.h>
 #include "uni64.h"
 int main()
 {
+ FILE *fp;
  int i;
+ fp = fopen("marsaglia2d.dat", "w");
  fillU(123456789,987654321);
- for (i=1;i<=500000;i++) printf("%20.16f %20.16f\n",uni64(),uni64());
+ for (i=1;i<=500000;i++) fprintf(fp,"%20.16f %20.16f\n",uni64(),uni64());
+ fclose(fp);
  return 0;
 }
 /*
 Local variables:
-  compile-command: "gcc -O0 -Wall -g -o marsaglia2d marsaglia2d.c uni64.c && ./marsaglia2d > marsaglia2d.dat && gnuplot marsaglia2d.gp"
+  compile-command: "gcc -O0 -Wall -g -o marsaglia2d marsaglia2d.c uni64.c && ./marsaglia2d && gnuplot marsaglia2d.gp"
 End:
 */
