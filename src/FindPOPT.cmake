30081d50d7c69dba30f4c2f412af4017cad09ea5
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2017-10-14 09:16:45 +0000

    learning cmake
    git-svn-id: https://svn.code.sf.net/p/loto/code/xtalgrowth/trunk@2979 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

---
+++ src/FindPOPT.cmake
@@ -0,0 +1,29 @@
+# Use pkg-config
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+  pkg_search_module(PC_POPT QUIET popt)
+endif()
+
+# Find the include directory
+find_path(
+  POPT_INCLUDE_PATH
+  NAMES popt.h
+  HINTS
+  ${PC_POPT_INCLUDEDIR}
+  ${PC_POPT_INCLUDE_DIRS}
+  DOC "Path containing popt.h")
+
+# Find the library directory
+find_library(
+  POPT_LIBRARY
+  NAMES popt
+  HINTS
+  ${PC_POPT_LIBRARYDIR}
+  ${PC_POPT_LIBRARY_DIRS}
+  DOC "Path containing libpopt.so, libpopt.dylib or so on")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  POPT
+  REQUIRED_VARS POPT_INCLUDE_PATH POPT_LIBRARY
+  VERSION_VAR PC_POPT_VERSION)
